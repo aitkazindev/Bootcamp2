58a957916b93353ba994ee71f8c9569b8e911016
Ibrahim <aitkazindev@gmail.com>
2024-10-13 15:45:42 +0900

    - Map, Filter, Sort, CompactMap

---
+++ Bootcamp2/Bootcamp213_SortMapFilter.swift
@@ -0,0 +1,18 @@
+//
+//  Bootcamp213_SortMapFilter.swift
+//  Bootcamp2
+//
+//  Created by Ibrahim Aitkazin on 13.10.2024.
+//
+
+import SwiftUI
+
+struct Bootcamp213_SortMapFilter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bootcamp213_SortMapFilter()
+}
